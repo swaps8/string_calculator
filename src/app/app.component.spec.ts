a1b4d2c8a10abc1c39ba23842ee82741ba75cef6
swaps8 <swapnil.n.jagtap8.com>
2024-09-14 16:30:05 +0530

    Add style and write a some testcases.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
     declarations: [AppComponent]
@@ -17,13 +19,42 @@ describe('AppComponent', () => {
   it(`should have as title 'string-calculator'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app.title).toEqual('string-calculator');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('string-calculator app is running!');
+  it('should return 0 if inputString is empty', () => {
+     const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.inputString = '';
+    app.calculateSum();
+    expect(app.result).toBe(0);
   });
+
+
+  it('should return 6 if inputString is "1\n2,3"', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+   const app = fixture.componentInstance;
+   app.inputString = "1\n2,3";
+   app.calculateSum();
+   expect(app.result).toBe(6);
+ });
+
+
+ it('should return -3 with error message if inputString is "//;\n1;2,-3"', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+ const app = fixture.componentInstance;
+ app.inputString = "//;\n1;2,-3";
+ app.calculateSum();
+ expect(app.result).toBe("Negative numbers not allowed -3");
+});
+
+it('Should return -2, -3 with error message if inputString is "//;\n-2,1;2,-3"',()=>{
+  const fixture = TestBed.createComponent(AppComponent);
+  const app = fixture.componentInstance;
+  app.inputString = "//;\n-2,1;2,-3";
+  app.calculateSum();
+  expect(app.result).toBe("Negative numbers not allowed -2, -3");
+})
+
 });
